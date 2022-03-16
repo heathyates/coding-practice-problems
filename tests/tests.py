7a7e9e81815fb5cff9698350f53be82df1a667ee
heath <heathyates@users.noreply.github.com>
2022-03-15 20:45:34 -0500

    Solved twosums and adding unit tests (broken)

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
-def inc(x):
-    return x + 1
+from coding_problems.two_sum import twoSum 
 
+def test_twoSum():
+    nums1 = [2, 7, 11, 15] # target = 9
+    nums2 = [3,2, 4] #target = 6
+    nums3 = [2,3,4] #target = 6
+    nums4 = [3,3] #target = 6
 
-def test_answer():
-    assert inc(3) == 5
+    assert twoSum(nums1, 9) == [0,1]
